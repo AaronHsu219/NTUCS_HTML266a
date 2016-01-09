1cc4e8c2f41e8881312525997d5f147a108d9b56
AaronHsu219 <civilboy1@gmail.com>
2016-01-09 23:06:05 +0800

    CL01

---
+++ CL01/student.js
@@ -0,0 +1,10 @@
+	var student = {
+		name: "Resnick",
+		contact: {
+			email:"resnick1223@gmail.com",
+			phone:"0910654720"
+		}
+	};
+
+student.name;
+student.contact.email;
---
+++ CL01/student2.js
@@ -0,0 +1,19 @@
+	var student = {
+		name: "Resnick",
+		contact: {
+			email:"resnick1223@gmail.com",
+			phone:"0910654720"
+		}
+	};
+
+student.name;
+student.contact.email;
+
+// php
+// student->contact->email;
+
+
+// stdents = [
+// 	{name: "Resnick, phone:""},
+// 	{name: "John", phone: ""}
+// ];
